02b94124fbe274ab7efb82931b295337181e078f
sweta AS <111897127+SwetaAS@users.noreply.github.com>
2024-03-29 08:56:05 +0530

    Delete route53 directory

--- route53/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_route53_zone" "route53" {
-  name = var.domain_name
-}
-
-resource "aws_acm_certificate" "cert" {
-  domain_name       =  var.domain_name
-  validation_method = "DNS"
-}
