e81453c667dfeae7b7d18ed76dc23d2905f9e886
sweta AS <111897127+SwetaAS@users.noreply.github.com>
2024-03-29 10:01:23 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-bucket"
+    bucket         = "terraform-bucket24"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
