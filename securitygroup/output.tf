634d72f5f7de22fa95e44ed6b6cbb685b2f21994
sweta AS <111897127+SwetaAS@users.noreply.github.com>
2024-03-29 08:56:18 +0530

    Delete securitygroup directory

--- securitygroup/output.tf
+++
@@ -1,3 +0,0 @@
-output "output_security_id" {
-  value = [aws_security_group.security_group.id]
-}
--- securitygroup/variable.tf
+++
@@ -1,9 +0,0 @@
-variable "ref_vpcid" {
-  
-}
-variable "securitygroup_name" {
-  type = string
-}
-variable "securitygroup_rules" {
-  type = map(any)
-}
